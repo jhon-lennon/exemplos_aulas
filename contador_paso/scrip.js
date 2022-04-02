abfadc81a4f736f5884e52cdc29985777226039c
jhon-lennon <jhoninfornet@gmail.com>
2022-04-02 18:02:45 -0300

    contador finalizado

---
+++ contador_paso/scrip.js
@@ -0,0 +1,30 @@
+
+function contar(){
+    var quant = document.getElementById('quant')
+    quant = Number(quant.value) + 1
+    var passo = document.getElementById('passo')
+    passo = Number(passo.value)
+    var res = document.getElementById('res')
+
+    if(passo < 1){
+        window.alert('o passa nao pode ser menor 1. continuar considerando passo 1')
+
+        passo = 1
+    }else if(quant < passo || quant < 1){
+        window.alert('a quantidade nao pode ser menor que o passo e nao deve estar vazia')
+        
+    }else{
+
+
+    var resutado = ''
+    console.log(`passo ${passo} quantidade ${quant}  `)
+   var i = 1
+   for(; i < quant; i += passo){
+
+    resutado +=`${i} \u{1F604} `
+    
+    }
+    res.innerText = resutado+`\u{1F3C1}`
+}
+
+}
